b9b65b80755f77e8a95bd5587c4ee1e629859f38
aathil1996 <ibnusafina@gmail.com>
2020-06-03 10:58:03 +0530

    Search bar created

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -3,5 +3,19 @@ import './BusinessList.css';
 import Business from '../Business/Business';
 
 class BusinessList extends React.Component{
-    
+    render(){
+        return (
+            <div className="BusinessList">
+                <Business />
+                <Business />
+                <Business />
+                <Business />
+                <Business />
+                <Business />
+               
+            </div>
+        )
+    }
 }
+
+export default BusinessList;
