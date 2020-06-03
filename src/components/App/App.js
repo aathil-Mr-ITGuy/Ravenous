e640f32ffdbd693afeb08af83e8f024aebfc49ef
aathil1996 <ibnusafina@gmail.com>
2020-06-03 11:06:46 +0530

    First part done

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from '../../logo.svg';
 import './App.css';
 import BusinessList from '../BusinessList/BusinessList';
 import SearchBar from '../SarchBar/SearchBar';
+import { render } from '@testing-library/react';
 
-function App() {
-  return ()
+
+class App extends Component{
+  render(){
+    return (
+      <div className="App">
+        <h1>ravenous</h1>
+        <SearchBar/>>
+        <BusinessList/>
+      </div>
+  
+    )
+  }
+
 }
 
 export default App;
